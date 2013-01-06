0e6e2de9f8c4af170ead8dd2353da445c71d2fe9
dogbox <dogbox@dogbox-VirtualBox.(none)>
2013-01-06 17:32:23 -0500

    started working on creating the form after selecting it's parameters in the form creator, have the question transfered to the form-created.html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request, render_template, url_for, redirect, flash
 
 
+
 app = Flask(__name__)
 app.secret_key = 'oyeah'
 
@@ -11,8 +12,24 @@ def index():
     if request.method == 'GET':
         return render_template("test-create.html")
     else:
-        question = request.form['question']
-        qtype = request.form['qtype']
-        length = request.form['length']
+       question = request.form['question']
+       #qtype = request.form['qtype']
+       #length = request.form['length']
+        
+   
+       return redirect(url_for('create_form',question = question))
         
         
+@app.route("/form")
+@app.route("/form/<question>", methods = ['POST','GET'])
+def create_form(question=None):
+    if request.method == "GET":
+        return render_template("created-form.html",question=question)
+    else:
+        return render_template("created-form.html", question=question)
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=5000)
