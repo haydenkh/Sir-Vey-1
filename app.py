071c08f8ddc74931830b4d1f00160e10b3afd78b
dogbox <dogbox@dogbox-VirtualBox.(none)>
2013-01-10 17:10:14 -0500

    app.py now saves a created form in the db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request, render_template, url_for, redirect, flash
 
+import db
 
 
 app = Flask(__name__)
@@ -11,11 +12,32 @@ app.secret_key = 'oyeah'
 def index():
     if request.method == 'GET':
         return render_template("test-create.html")
+
     else:
+        
        question = request.form['question']
        qtype = request.form['qtype']
        length = request.form['length']
-        
+       #if anon isn't checked set to null
+       try:
+           anon = request.form['anon']
+       except:
+           anon = ''
+       answers = []
+
+       i = 1
+       if qtype =='mc':
+           #iterate through answers written depending on length
+           while(i < int(length) + 1):
+               answer = request.form['Answer%d' %i]
+               print answer
+               answers.append(str(answer))
+               i = i+1
+       print "HERE:"
+
+       db.add_form(question,qtype,answers,anon)
+       print db.get_question_id(question)
+
        return redirect(url_for('create_form',question=question))
         
         
@@ -24,7 +46,7 @@ def index():
 def create_form(question=None):
     #get questions variables here and use them
     if request.method == "GET":
-        return render_template("created-form.html",question=question,qtype="text")
+        return render_template("created-form.html",question=question,qtype=db.get_qtype(question))
 
         
 
